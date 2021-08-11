eb6f95b2db6dac04974fd00a72fa346f2a890650
Anatolii Afarinov <aafarinov@gmail.com>
2021-08-11 16:59:23 +0300

    lesson01

---
+++ lesson01.playground/Contents.swift
@@ -0,0 +1,40 @@
+//20
+//23.6
+//45.6789
+//"Hello"
+//
+//let redColor = "red"
+//let зеленыйЦвет = "зеленый"
+//var color = "black"
+//
+//color = redColor
+
+let redColor = "red"
+let greenColor = "green"
+let blueColor = "blue"
+
+var color = "black"
+color = redColor
+color
+color = greenColor
+color
+color = "abracadabra"
+
+color
+
+
+print(color)
+
+print(redColor)
+print(greenColor)
+print(blueColor)
+
+print("red color value is \(redColor)")
+
+
+/*
+ ДЗ к уроку №1
+ создать характеристики студента через константы (имя, фамилия, рост, вес, возраст)
+ 
+ */
+
